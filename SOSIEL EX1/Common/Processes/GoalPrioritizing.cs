14ca2b47dee79bd5c157b497629a146d45a9083a
Aleksandr.Karifanov <aleksandr.karifanov@effective-soft.com>
2018-07-06 12:38:40 +0300

    review goal prioritizing, change calculations of Relative difference between goal value and focal goal value

--- SOSIEL EX1/Common/Processes/GoalPrioritizing.cs
+++ SOSIEL EX1/Common/Processes/GoalPrioritizing.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Common.Entities;
+using Common.Exceptions;
 using Common.Helpers;
 
 namespace Common.Processes
@@ -27,11 +28,11 @@ namespace Common.Processes
                 if (noConfidenceGoals.Length > 0 && agent.Prototype.UseImportanceAdjusting)
                 {
                     var noConfidenceProportions = noConfidenceGoals.Select(kvp => new
-                        {
-                            Proportion = kvp.Value.Importance *
-                                         (1 + CalculateNormalizedValue(agent, kvp.Key, kvp.Value)),
-                            Goal = kvp.Key
-                        })
+                    {
+                        Proportion = kvp.Value.Importance *
+                                         (1 + CalculateRelativedDifference(agent, kvp.Key, kvp.Value)),
+                        Goal = kvp.Key
+                    })
                         .ToArray();
 
                     var confidenceGoals = goals.Where(kvp => kvp.Value.Confidence).ToArray();
@@ -41,11 +42,11 @@ namespace Common.Processes
                     double totalNoConfidenceAdjustedProportions = noConfidenceProportions.Sum(p => p.Proportion);
 
                     var confidenceProportions = confidenceGoals.Select(kvp => new
-                        {
-                            Proportion = kvp.Value.Importance * (1 - totalNoConfidenceAdjustedProportions) /
+                    {
+                        Proportion = kvp.Value.Importance * (1 - totalNoConfidenceAdjustedProportions) /
                                          totalConfidenceUnadjustedProportions,
-                            Goal = kvp.Key
-                        })
+                        Goal = kvp.Key
+                    })
                         .ToArray();
 
                     Enumerable.Concat(noConfidenceProportions, confidenceProportions)
@@ -72,11 +73,8 @@ namespace Common.Processes
         /// <param name="goal"></param>
         /// <param name="goalState"></param>
         /// <returns></returns>
-        double CalculateNormalizedValue(IAgent agent, Goal goal, GoalState goalState)
+        double CalculateRelativedDifference(IAgent agent, Goal goal, GoalState goalState)
         {
-            double maxPossibleDifference = 0;
-
-
             DecisionOptionLayerConfiguration layerConfiguration = agent.AssignedDecisionOptions
                 .Where(kh => kh.Consequent.Param == goal.ReferenceVariable &&
                              (kh.Layer.LayerConfiguration.ConsequentValueInterval != null &&
@@ -86,32 +84,38 @@ namespace Common.Processes
 
             if (layerConfiguration != null)
             {
-                maxPossibleDifference = Math.Max(Math.Abs(layerConfiguration.MaxValue(agent) - goalState.Value),
-                    Math.Abs(layerConfiguration.MinValue(agent) - goalState.Value));
-            }
-            else
-            {
-                if (goal.Tendency == "EqualToOrAboveFocalValue" || goal.Tendency == "Maximize")
+                if (goal.Tendency == "Maximize")
                 {
-                    maxPossibleDifference = (string.IsNullOrEmpty(goal.FocalValueReference)
-                        ? goalState.FocalValue
-                        : (double) agent[goal.FocalValueReference]);
+                    var maxGoalValue = agent.AssignedDecisionOptions
+                        .Where(kh => kh.Consequent.Param == goal.ReferenceVariable)
+                        .Select(kh => string.IsNullOrEmpty(kh.Consequent.VariableValue)
+                            ? (double)kh.Consequent.Value
+                            : (double)agent[kh.Consequent.VariableValue])
+                        .Max();
+
+                    return Math.Abs(goalState.DiffCurrentAndFocal / (goalState.FocalValue - maxGoalValue));
                 }
+
+                if (goal.Tendency == "EqualToOrAboveFocalValue")
+                {
+                    var minGoalValue = 0;
+
+                    return Math.Abs(goalState.DiffCurrentAndFocal / (goalState.FocalValue - minGoalValue));
+                }
+
 
                 if (goal.Tendency == "Minimize")
                 {
-                    double maxValue = agent.AssignedDecisionOptions
-                        .Where(kh => kh.Consequent.Param == goal.ReferenceVariable)
-                        .Select(kh => string.IsNullOrEmpty(kh.Consequent.VariableValue)
-                            ? (double) kh.Consequent.Value
-                            : (double) agent[kh.Consequent.VariableValue])
-                        .Max();
 
-                    maxPossibleDifference = maxValue - goalState.PriorValue;
                 }
+
+
+                throw new SosielAlgorithmException(
+                    "Cannot calculate relative difference between goal value and focal goal value for tendency" +
+                    goal.Tendency);
             }
 
-            return Math.Abs(goalState.DiffCurrentAndPrior / maxPossibleDifference);
+            throw new SosielAlgorithmException("Please fill out section layer configuration");
         }
     }
 }
